915e8d62e3090a86b8b376dc36b039428de6ef33
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2025-10-09 14:42:47 -0300

    Make base character class Abstract. Remove functions that won't be used in AI characters.

--- Source/AlienJourney/Private/Character/AlienJBaseCharacter.cpp
+++ Source/AlienJourney/Private/Character/AlienJBaseCharacter.cpp
@@ -7,7 +7,7 @@
 AAlienJBaseCharacter::AAlienJBaseCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -17,18 +17,3 @@ void AAlienJBaseCharacter::BeginPlay()
 	Super::BeginPlay();
 	
 }
-
-// Called every frame
-void AAlienJBaseCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AAlienJBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
--- Source/AlienJourney/Public/Character/AlienJBaseCharacter.h
+++ Source/AlienJourney/Public/Character/AlienJBaseCharacter.h
@@ -8,26 +8,23 @@
 
 /*
  * Base class for Player and AI/NPC characters.
+ * Make it Abstract to prevent it from being used in levels.
  */
 
-UCLASS()
+UCLASS(Abstract)
 class ALIENJOURNEY_API AAlienJBaseCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+	
 	AAlienJBaseCharacter();
 
 protected:
-	// Called when the game starts or when spawned
+
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:	
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
